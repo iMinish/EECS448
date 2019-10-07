5d321f7e894ff2ba4b4a186dcb2c9db583eb8b8a
Rob Chirpich <rob.chirpich@gmail.com>
2019-10-07 18:12:47 -0500

    Add files via upload

--- Lab04/Exercise3/customerBackend.php
+++ Lab04/Exercise3/customerBackend.php
@@ -1,4 +1,9 @@
 <?php
+  error_reporting(E_ALL);
+  ini_set("display_errors", 1);
+
+  echo "<link rel='stylesheet' type='text/css' href='style.css'/>";
+
   $username = $_POST["username"];
   $password = $_POST["password"];
   $burgerQuantity = $_POST["burger"];
@@ -11,16 +16,47 @@
   $shippingCost = 0;
   $totalCost = 0;
 
-  if($shipping == "seven") {
+  if($shippingOption == "seven") {
     $shippingCost = 0;
-  } else if($shipping == "overnight") {
+    $totalCost = $burgerCost + $chickenCost + $waterCost + $shippingCost;
+
+    echo "<div align=center><br>Welcome to Rob's EECS448 Web Store, $username<br>";
+    echo "Your password is: $password<br><br>";
+    echo "Here is your Receipt:<br><br>";
+
+    echo "You purchased $burgerQuantity Burgers for $5.00 each. Burger Sub-Total is $$burgerCost.00<br>";
+    echo "You purchased $chickenQuantity Chicken Tenders and Fries for $10.00 each. Chicken Tenders and Fries Sub-Total is $$chickenCost.00<br>";
+    echo "You purchased $waterQuantity Waters for $15.00 each. Water Sub-Total is $$waterCost.00<br><br>";
+
+    echo "You picked Seven Day Shipping at a rate of $$shippingCost.00<br><br>";
+    echo "Your Total Cost is $$totalCost.00<br></div>";
+  } else if($shippingOption == "overnight") {
     $shippingCost = 50;
-  } else if($shipping == "three") {
+    $totalCost = $burgerCost + $chickenCost + $waterCost + $shippingCost;
+
+    echo "<div align=center><br>Welcome to Rob's EECS448 Web Store, $username<br>";
+    echo "Your password is: $password<br><br>";
+    echo "Here is your Receipt:<br><br>";
+
+    echo "You purchased $burgerQuantity Burgers for $5.00 each. Burger Sub-Total is $$burgerCost.00<br>";
+    echo "You purchased $chickenQuantity Chicken Tenders and Fries for $10.00 each. Chicken Tenders and Fries Sub-Total is $$chickenCost.00<br>";
+    echo "You purchased $waterQuantity Waters for $15.00 each. Water Sub-Total is $$waterCost.00<br><br>";
+
+    echo "You picked Overnight Shipping at a rate of $$shippingCost.00<br><br>";
+    echo "Your Total Cost is $$totalCost.00<br></div>";
+  } else if($shippingOption == "three") {
     $shippingCost = 5;
+    $totalCost = $burgerCost + $chickenCost + $waterCost + $shippingCost;
+
+    echo "<div align=center><br>Welcome to Rob's EECS448 Web Store, $username<br>";
+    echo "Your password is: $password<br><br>";
+    echo "Here is your Receipt:<br><br>";
+
+    echo "You purchased $burgerQuantity Burgers for $5.00 each. Burger Sub-Total is $$burgerCost.00<br>";
+    echo "You purchased $chickenQuantity Chicken Tenders and Fries for $10.00 each. Chicken Tenders and Fries Sub-Total is $$chickenCost.00<br>";
+    echo "You purchased $waterQuantity Waters for $15.00 each. Water Sub-Total is $$waterCost.00<br><br>";
+
+    echo "You picked Three Day Shipping at a rate of $$shippingCost.00<br><br>";
+    echo "Your Total Cost is $$totalCost.00<br></div>";
   }
-
-  $totalCost = $burgerCost + $chickenCost + $waterCost + $shippingCost;
-
-  echo "Welcome to Rob's EECS448 Web Store, " .$username.;
-  echo "Your password is: " .$password.;
 ?>
