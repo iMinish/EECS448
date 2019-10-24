d0dd03c4a513b73295d022edf0466014a68acea3
Rob Chirpich <rob.chirpich@gmail.com>
2019-10-24 14:13:20 -0500

    Update ViewUsers.php

--- Lab05/Exercise5/ViewUsers.php
+++ Lab05/Exercise5/ViewUsers.php
@@ -1,1 +1,27 @@
+<?php
+  print "<h1>List of Users</h1>";
+  print "<table border=\"1\">";
+  print "<tr><th>Username:</th></tr>";
 
+  $mysqli = new mysqli("mysql.eecs.ku.edu", "rob.chirpich", "Ah4poo4a", "robchirpich");
+
+  /* check connection */
+  if($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+  }
+
+  $query = "SELECT * FROM Users";
+
+  if($result = $mysqli->query($query)) {
+    /* fetch associative array */
+    while($row = $result->fetch_assoc()) {
+        print "<tr><td>" .$row["user_id"]. "</td></tr>";
+    }
+
+    $result->free(); /* free result set */
+  }
+
+  $mysqli->close(); /* close connection */
+  print "</table>";
+?>
