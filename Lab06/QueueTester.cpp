0426c70915cb86b2f339e90debf183006ea21a10
Rob Chirpich <rob.chirpich@gmail.com>
2019-11-13 22:03:27 -0600

    Update QueueTester.cpp

--- Lab06/QueueTester.cpp
+++ Lab06/QueueTester.cpp
@@ -19,6 +19,9 @@ void QueueTester::runTests()
   test08();
   test09();
   test10();
+  test11();
+  test12();
+  test13();
 }
 
 void QueueTester::test01()
@@ -138,3 +141,36 @@ void QueueTester::test10()
   delete testQueue;
 }
 
+void QueueTester::test11()
+{
+  Queue* testQueue = new Queue();
+  try
+    testQueue->peekFront();
+  catch(const std::exception& e)
+    std::cout << "[Test #11] ERROR... " << e.what() << ": PASSED\n";
+  delete testQueue;
+}
+
+void QueueTester::test12()
+{
+  Queue* testQueue = new Queue();
+  try
+    testQueue->dequeue();
+  catch(const std::exception& e)
+    std::cout << "[Test #12] ERROR... " << e.what() << ": PASSED\n";
+  delete testQueue;
+}
+
+void QueueTester::test13()
+{
+  Queue* testQueue = new Queue();
+  for(int i = 1; i <= 10; i++)
+    testQueue->enqueue(i); // BACK OF QUEUE [1,2,3,4,5,6,7,8,9,10] FRONT OF QUEUE
+  for(int i = 1; i <= 5; i++)
+    testQueue->dequeue(); // BACK OF QUEUE [1,2,3,4,5] FRONT OF QUEUE
+  if(testQueue->peekFront() == 5)
+    std::cout << "[Test #13] Enqueue 10 values and dequeue 5 values then peekFront returns the last element in the Queue: PASSED\n";
+  else
+    std::cout << "[Test #13] Enqueue 10 values and dequeue 5 values then peekFront returns the last element in the Queue: FAILED\n";
+  delete testQueue;
+}
